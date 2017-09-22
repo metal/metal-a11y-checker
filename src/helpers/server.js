3e2d249b60b3c79237030ab08632942df8eaf420
Bence Varga <vbence86@gmail.com>
2017-09-22 10:47:26 +0200

    add eslint and prettier

--- src/helpers/server.js
+++ src/helpers/server.js
@@ -1,39 +1,39 @@
-import { Promise } from 'es6-promise';
-import express from 'express';
+import { Promise } from "es6-promise";
+import express from "express";
 
 class Server {
-	/**
+  /**
 	 * Starts the express server
 	 * @param {number} port
 	 * @param {string} dir
 	 * @return {Promise}
 	 * @throw {Error}
 	 */
-	start(port, dir) {
-		if (this.app) throw new Error('Server is already running!');
+  start(port, dir) {
+    if (this.app) throw new Error("Server is already running!");
 
-		if (!port) port = 9000;
-		if (!dir) dir = './';
+    if (!port) port = 9000;
+    if (!dir) dir = "./";
 
-		this.app = express();
-		this.app.use(express.static(dir));
-		return new Promise((resolve, reject) => {
-			this.server = this.app.listen(port, function(err) {
-				if (err) reject(err);
-				console.log(`Server is listening on port ${port} hosting: ${dir}`);
-				resolve({ port, dir });
-			});
-		});
-	}
+    this.app = express();
+    this.app.use(express.static(dir));
+    return new Promise((resolve, reject) => {
+      this.server = this.app.listen(port, function(err) {
+        if (err) reject(err);
+        console.log(`Server is listening on port ${port} hosting: ${dir}`);
+        resolve({ port, dir });
+      });
+    });
+  }
 
-	/**
+  /**
 	 * Stops the server
 	 * @return {Promise}
 	 */
-	stop() {
-		return Promise.resolve().then(() => this.server.close());
-	}
-};
+  stop() {
+    return Promise.resolve().then(() => this.server.close());
+  }
+}
 
 export { Server };
 export default Server;
